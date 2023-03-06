adcda385ebc42f5fd92e792ba60286752a3867de
Nikita Lukyanov <119298712+NikitaLukyanov25@users.noreply.github.com>
2023-03-06 13:01:05 +0300

    Delete ApiPets.py

--- ApiPets.py
+++
@@ -1,9 +0,0 @@
-import requests
-
-status='available'
-res = requests.get( f"https://petstore.swagger.io/v2/pet/findByStatus?status={status}", headers = {'accept': 'application/json'})
-
-print(res.json())
-print(res.text)
-print(res.status_code)
-print(type(res.json()))
